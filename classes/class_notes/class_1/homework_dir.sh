38070a2bd00856a45e7407be627903536fffe3f5
lromang <luis.roangarci@gmail.com>
2022-01-27 22:02:16 -0600

    assignments

---
+++ classes/class_notes/class_1/homework_dir.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# ----------------------------------------
+# Con este oneliner, extraje los nombres
+# de los equipos y generé un directorio
+# con el mismo nombre dentro de homework.
+# ¿Qué es lo que hace mi expresión regular?
+# ¿Por qué es necesario el primer ^?
+# piensa en el string: aaa.bbb.ccc
+# ----------------------------------------
+
+for students in $(ls ../../course_generals/teams/ | grep -Eo '^[^\.]+' ); do mkdir $students; done
