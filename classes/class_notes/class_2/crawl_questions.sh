0298e95e313d8ccff73c31722f78b5817fb74f7c
lromang <luis.roangarci@gmail.com>
2022-02-03 21:05:14 -0600

    homework 3

--- classes/class_notes/class_2/crawl_questions.sh
+++ classes/class_notes/class_2/crawl_questions.sh
@@ -4,11 +4,12 @@
 # This script extracts a list of relevant
 # questions from stack exchange.
 # 1.- Iterate over n_pages and download the html. (due for Thursday 2022-02-03)
+# https://worldbuilding.stackexchange.com/questions?tab=newest&page=1
 #----------------------------------------
 
 n_pages="${1:-10}"
 
 for page in $(seq $n_pages)
 do
-    # Your code goes here
+    curl "https://worldbuilding.stackexchange.com/questions?tab=newest&page=$page" >>  stack_ex.txt;
 done
