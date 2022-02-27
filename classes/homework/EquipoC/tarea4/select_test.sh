958ca834f30fa9bff762cd1dd77fc90e02fd857e
FranceSheep <81389205+FranceSheep@users.noreply.github.com>
2022-02-27 14:13:57 -0600

    select_test.sh

--- classes/homework/EquipoC/tarea4/select_test.sh
+++ classes/homework/EquipoC/tarea4/select_test.sh
@@ -11,15 +11,22 @@
 # R1: Done so Alonso
 # TASK 2: Think of an application of this principle.
 # R2: The first application for the pipeline within function
-#	that we can think of is that unlike sed or grep, you
-#	can call jq only once and within arrange your query
-#	as you like.
+#	  that we can think of is that unlike sed or grep, you
+#	  can call jq only once and within arrange your query
+#	  as you like.
 # TASK 3: Take a look at the commented script. It is
 #         yet a more challenging application.
 #         Explain what each chunk of code is doing.
 # R3: The first chunk is the cat, you need this to know the
-#	file in which you will be working. The second is already
-#	a jq that 
+#	  file in which you will be working. The second is already
+#	  a jq that does a lot of things: first it organizes the
+#   contnents into a [], then it separates each pair of key and 
+#   value from othes pairs of keys and values, but since this
+#   circles everything onto a [[]], we need .[] to remove one [],
+#   at the endo of this jq we have select(.value | test("^v.*", "ix"))]'
+#   where the select searches for every pair of key-value where the regex 
+#   of the value starts whit the char 'v' or contains the string "ix".
+#   Finaly we reunite each key-value pair that the previous jq throwed.
 # cat basic_test.json | jq '[. | to_entries | .[] | select(.value | test("^v.*", "ix"))]' | jq 'from_entries'
 # ----------------------------------------
 
